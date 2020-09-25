6a65a4928bb467dc7911c145af3906959e2cf778
Kristina <christychri0@gmail.com>
2020-09-25 18:10:53 +0700

    добавила первую модель (tasks)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import {generateTask} from "./mock/task";
 import {generateFilter} from "./mock/filter.js";
 import BoardPresenter from "./presenter/board.js";
 import {render, RenderPosition} from "./utils/render.js";
+import TasksModel from "./model/tasks.js";
 
 const TASKS_COUNT = 22;
 
@@ -11,10 +12,13 @@ const TASKS_COUNT = 22;
 const tasks = new Array(TASKS_COUNT).fill().map(generateTask);
 const filters = generateFilter(tasks);
 
+const tasksModel = new TasksModel();
+tasksModel.setTasks(tasks);
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement);
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
 render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
