b0291d91d4b6bde14f26b7af1aa0f9447767d28b
Kristina <christychri0@gmail.com>
2020-09-28 14:56:34 +0700

    добавили презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import {generateTask} from "./mock/task";
+import {render, RenderPosition} from "./utils/render.js";
 import SiteMenuView from "./view/site-menu.js";
 import FilterView from "./view/filter.js";
-import {generateTask} from "./mock/task";
 import BoardPresenter from "./presenter/board.js";
-import {render, RenderPosition} from "./utils/render.js";
+import FilterPresenter from "./presenter/filter.js";
 import TasksModel from "./model/tasks.js";
 import FilterModel from "./model/filter.js";
 
@@ -10,13 +11,6 @@ const TASKS_COUNT = 22;
 
 
 const tasks = new Array(TASKS_COUNT).fill().map(generateTask);
-const filters = [
-  {
-    type: `all`,
-    name: `ALL`,
-    count: 0
-  }
-];
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -26,9 +20,10 @@ const filterModel = new FilterModel();
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
-
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterView(filters), RenderPosition.BEFOREEND);
 
+const boardPresenter = new BoardPresenter(siteMainElement, tasksModel);
+const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
+
+filterPresenter.init();
 boardPresenter.init();
