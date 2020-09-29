2ece31f5389074566f5c6776271cede1bfff764a
Kristina <christychri0@gmail.com>
2020-09-29 18:41:23 +0700

    настроили график по датам

--- src/view/statistic.js
+++ src/view/statistic.js
@@ -3,7 +3,15 @@ import Chart from "chart.js";
 import ChartDataLabels from 'chartjs-plugin-datalabels';
 import SmartView from "./smart.js";
 import {getCurrentDate} from "../utils/task.js";
-import {countCompletedTaskInDateRange,makeItemsUniq, countTasksByColor, colorToHex} from "../utils/statistic.js";
+import {
+  countCompletedTaskInDateRange,
+  makeItemsUniq,
+  countTasksByColor,
+  colorToHex,
+  countTasksInDateRange,
+  parseChartDate,
+  getDatesInRange
+} from "../utils/statistic.js";
 
 const renderColorsChart = (colorsCtx, tasks) => {
   // Функция для отрисовки графика по цветам
@@ -68,14 +76,18 @@ const renderColorsChart = (colorsCtx, tasks) => {
 };
 
 const renderDaysChart = (daysCtx, tasks, dateFrom, dateTo) => {
+  const dates = getDatesInRange(dateFrom, dateTo);
+  const parsedDates = dates.map(parseChartDate);
+  const taskInDateRangeCounts = countTasksInDateRange(dates, tasks);
+
   // Функция для отрисовки графика по датам
   return new Chart(daysCtx, {
     plugins: [ChartDataLabels],
     type: `line`,
     data: {
-      labels: [`3 Sep`], // Сюда нужно передать названия дней
+      labels: parsedDates, // Сюда нужно передать названия дней
       datasets: [{
-        data: [1], // Сюда нужно передать в том же порядке количество задач по каждому дню
+        data: taskInDateRangeCounts, // Сюда нужно передать в том же порядке количество задач по каждому дню
         backgroundColor: `transparent`,
         borderColor: `#000000`,
         borderWidth: 1,
