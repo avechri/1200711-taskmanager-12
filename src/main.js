fff9336f9bb01e164ac2e0a09f297a6fb1a1102f
Kristina <christychri0@gmail.com>
2020-09-29 15:32:18 +0700

    запрограммировали меню приложения

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {generateTask} from "./mock/task";
 import {render, RenderPosition} from "./utils/render.js";
+import {MenuItem} from "./const";
 import SiteMenuView from "./view/site-menu.js";
 import BoardPresenter from "./presenter/board.js";
 import FilterPresenter from "./presenter/filter.js";
@@ -18,11 +19,33 @@ const filterModel = new FilterModel();
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
+const siteMenuComponent = new SiteMenuView();
 
 render(siteHeaderElement, new SiteMenuView(), RenderPosition.BEFOREEND);
 
 const boardPresenter = new BoardPresenter(siteMainElement, tasksModel, filterModel);
 const filterPresenter = new FilterPresenter(siteMainElement, filterModel, tasksModel);
+
+const handleSiteMenuClick = (menuItem) => {
+  switch (menuItem) {
+    case MenuItem.ADD_NEW_TASK:
+      // Скрыть статистику
+      // Показать доску
+      // Показать форму добавления новой задачи
+      // Убрать выделение с ADD NEW TASK после сохранения
+      break;
+    case MenuItem.TASKS:
+      // Показать доску
+      // Скрыть статистику
+      break;
+    case MenuItem.STATISTICS:
+      // Скрыть доску
+      // Показать статистику
+      break;
+  }
+};
+
+siteMenuComponent.setMenuClickHandler(handleSiteMenuClick);
 
 document.querySelector(`#control__new-task`).addEventListener(`click`, (evt) => {
   evt.preventDefault();
