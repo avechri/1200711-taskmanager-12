d24f8cd8b63464ce7610edf9c1110f99cdbe82fd
Kristina <christychri0@gmail.com>
2020-10-19 18:55:08 +0700

    обучили презентер слушать событие init

--- src/presenter/board.js
+++ src/presenter/board.js
@@ -1,6 +1,7 @@
 import BoardView from "../view/board.js";
 import SortView from "../view/sort.js";
 import TaskListView from "../view/task-list.js";
+import LoadingView from "../view/loading.js";
 import NoTaskView from "../view/no-tasks.js";
 import LoadMoreButtonView from "../view/load-more-button.js";
 import TaskPresenter from "./task.js";
@@ -20,6 +21,7 @@ export default class Board {
     this._renderedTaskCount = TASK_COUNT_PER_STEP;
     this._currentSortType = SortType.DEFAULT;
     this._taskPresenter = {};
+    this._isLoading = true;
 
     this._sortComponent = null;
     this._loadMoreButtonComponent = null;
@@ -27,6 +29,7 @@ export default class Board {
     this._boardComponent = new BoardView();
     this._taskListComponent = new TaskListView();
     this._noTaskComponent = new NoTaskView();
+    this._loadingComponent = new LoadingView();
 
     this._handleViewAction = this._handleViewAction.bind(this);
     this._handleModelEvent = this._handleModelEvent.bind(this);
@@ -97,6 +100,11 @@ export default class Board {
       case UserAction.DELETE_TASK:
         this._tasksModel.deleteTask(updateType, update);
         break;
+      case UpdateType.INIT:
+        this._isLoading = false;
+        remove(this._loadingComponent);
+        this._renderBoard();
+        break;
     }
   }
 
@@ -147,6 +155,10 @@ export default class Board {
     tasks.forEach((task) => this._renderTask(task));
   }
 
+  _renderLoading() {
+    render(this._boardComponent, this._loadingComponent, RenderPosition.AFTERBEGIN);
+  }
+
   _renderNoTasks() {
     render(this._boardComponent, this._noTaskComponent, RenderPosition.AFTERBEGIN);
   }
@@ -182,6 +194,7 @@ export default class Board {
     this._taskPresenter = {};
 
     remove(this._sortComponent);
+    remove(this._loadingComponent);
     remove(this._noTaskComponent);
     remove(this._loadMoreButtonComponent);
 
@@ -200,6 +213,11 @@ export default class Board {
   }
 
   _renderBoard() {
+    if (this._isLoading) {
+      this._renderLoading();
+      return;
+    }
+
     const tasks = this._getTasks();
     const taskCount = tasks.length;
 
